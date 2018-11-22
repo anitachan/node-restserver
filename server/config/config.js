e284ac215a48614b1cbce46f0ed6a29b2e4e1760
Anita <anita_chavez@hotmail.com>
2018-11-22 14:54:05 -0500

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -1,24 +1,38 @@
-//======
+//=====================
 //Puerto
-//======
+//=====================
 process.env.PORT = process.env.PORT || 3000;
 
-//==============
+//=====================
 //Entorno
-//==============
+//=====================
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-//==============
-//Base de Datos
-//==============
+//=====================
+//Vencimiento token
+//=====================
+//60 segundos
+//60 minutos
+//24 horas 
+//30 dias
 
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30
+
+//=====================
+//SEED de autenticación
+//=====================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+//=====================
+//Base de Datos
+//=====================
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafeUser:a12345@ds139715.mlab.com:39715/cafe';
+    urlDB = 'mongodb://cafeuser:123456a@ds111244.mlab.com:11244/cafedb';
 }
 
 process.env.URLDB = urlDB;
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const app = express();
-const Usuario = require('../models/usuario')
+const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_role } = require('../middleware/autenticacion');
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -33,7 +34,7 @@ app.get('/usuario', function(req, res) {
         });
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_role], (req, res) => {
     let body = req.body;
     let usuario = new Usuario({
         nombre: body.nombre,
@@ -59,7 +60,7 @@ app.post('/usuario', function(req, res) {
     });
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_role], function(req, res) {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
@@ -79,7 +80,7 @@ app.put('/usuario/:id', function(req, res) {
 });
 
 //Borrado lógico
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_role], function(req, res) {
     let id = req.params.id;
     let cambiaEstado = {
         estado: false
