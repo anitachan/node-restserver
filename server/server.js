5300430567f5571265145e200e658ac12aae52b7
Anita <anita_chavez@hotmail.com>
2018-11-22 09:57:30 -0500

    Rest Server usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,28 +11,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario');
-})
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    res.json({
-        persona: body
-    });
-})
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario');
-})
-
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Base de datos ONLINE');
+    }
+});
+mongoose.set('useCreateIndex', true);
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
