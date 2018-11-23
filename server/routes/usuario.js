23b28a14007b81985f4ff8dc29a50cbc8ab08538
Anita  Chávez <anitachavez@MacBook-Pro-de-Anita.local>
2018-11-22 20:24:00 -0500

    Correccion token

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -34,8 +34,8 @@ app.get('/usuario', verificaToken, (req, res) => {
         });
 });
 
-//app.post('/usuario', [verificaToken, verificaAdmin_role], (req, res) => {
-app.post('/usuario', (req, res) => {
+app.post('/usuario', [verificaToken, verificaAdmin_role], (req, res) => {
+    //app.post('/usuario', (req, res) => {
     let body = req.body;
     let usuario = new Usuario({
         nombre: body.nombre,
